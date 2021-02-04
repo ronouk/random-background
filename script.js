9f10bf2f364ec5823473ac8fa6982c2a06a2ae55
A K M Saiful Islam <shubro42@yahoo.com>
2021-02-04 20:05:53 +0600

    hexcode less than six digit problem fixed

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-document.getElementById('button').addEventListener('click', function(){
-    const background = '#' + Math.round( Math.random()*999999);
-    document.getElementById('body').style.backgroundColor = background;
-    document.getElementById('button-area').style.backgroundColor= background;
-    document.getElementById('current-color').innerText = background;
+const bgChange = document.getElementById('button').addEventListener('click', function () {
+
+  const background = '#' + Math.round(Math.random() * 1000000);
+
+  if (background.length == 7) {
+    bgColor(background);
+  } else {
+    bgColor('#748276');
+  }
+
+  // Function for color change
+
+  function bgColor(color) {
+    document.getElementById('body').style.backgroundColor = color;
+    document.getElementById('button-area').style.backgroundColor = color;
+    document.getElementById('current-color').innerText = color;
+  }
+
 })
